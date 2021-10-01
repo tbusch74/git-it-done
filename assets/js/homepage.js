98152ace8e481e6759b7755e8a78bb13166f1260
Tony Busch <abusch@css.edu>
2021-09-30 22:48:25 -0500

    set up api call

---
+++ assets/js/homepage.js
@@ -0,0 +1,8 @@
+var getUserRepos = function() {
+    var response = fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);        
+        });
+    });
+}
+getUserRepos();
